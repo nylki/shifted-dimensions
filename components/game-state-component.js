7edf8a6f220a309c105c25a7fa688fd98942a317
Tom <tombrwe@gmail.com>
2017-08-30 11:26:09 +0200

    added sound; improved shader

--- components/game-state-component.js
+++ components/game-state-component.js
@@ -6,17 +6,50 @@
 
 export {gameStateSystem};
 
+const maxFreq = 300;
+const maxVol = 0.1;
+
 
 let gameStateSystem = AFRAME.registerSystem('game-state', {
   schema: {
     level: {type: 'number', default: 1}
   },
   init: function () {
-
+    
+    this.sceneEl = document.querySelector('a-scene');
+    this.camera = document.getElementById('camera');
+    this.lostStone = document.getElementById('lostStone');
+    console.log(this.lostStone);
+    this.magicLight = document.querySelector('#laser');
+    
+    // Pre-create some vectors to avoid doing that in tick()'s
+    this.dirLightStone = new THREE.Vector3();
+    
+    // Init audio foo
+    this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    
+    this.oscillator = this.audioCtx.createOscillator();
+    this.oscillator.type = 'square'; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
+    this.oscillator.frequency.value = 0; // value in hertz
+    this.oscillator.detune.value = 100;
+    this.oscillator.start();
+    
+    this.gainNode = this.audioCtx.createGain();
+    
+    this.oscillator.connect(this.gainNode);
+    this.gainNode.connect(this.audioCtx.destination);
+    
+    
+    
     
   },
-  tick: function () {
+  tick: function (time, timeDelta) {
+    
+    let vel = this.lostStone.components['physics-body'].velocity.length();
+    this.dirLightStone.copy(this.camera.object3D.position).sub(this.lostStone.object3D.position);
+    this.oscillator.frequency.value = (Math.sin(time * 0.0005) * maxFreq) * (vel * 100.0);
 
+    this.gainNode.gain.value = (1.0 / this.dirLightStone.length()) * 1.0 * maxVol;
   }
-
+  
 });
--- components/labyrinth-material-component.js
+++ components/labyrinth-material-component.js
@@ -12,19 +12,24 @@ void main() {
   gl_Position = v;
 }
 `;
+
 const fragmentShader = `
 uniform float u_time;
 uniform vec2 u_resolution;
 uniform vec3 u_controllerPos;
+uniform vec3 u_stonePos;
 
 varying vec4 worldPosition;
 
 void main(void)
 {
-  vec3 dir = u_controllerPos - worldPosition.xyz;
-  float dist = length(dir);
-	vec2 uv = gl_FragCoord.xy / u_resolution.xy;
-	gl_FragColor = vec4(worldPosition.xyz, dist * 0.1);
+  vec3 controllerDir = u_controllerPos - worldPosition.xyz;
+  vec3 stoneDir = u_stonePos - worldPosition.xyz;
+  float dist = length(controllerDir) * 0.15;
+  float stoneDist = length(stoneDir) * 0.3;
+	// vec2 uv = gl_FragCoord.xy / u_resolution.xy;
+  vec3 color = vec3(0.0, 0.0, 0.0);
+	gl_FragColor = vec4(vec3(1.0, 0.0, 0.0), 1.0 - stoneDist);
   
 }
 `;
@@ -35,19 +40,18 @@ let labyrinthMaterial = AFRAME.registerComponent('labyrinth-material', {
   },
   init: function () {
     this.sceneEl = document.querySelector('a-scene');
-    this.gameState = this.system = this.sceneEl.systems['game-state'];
-    this.magicLight = document.querySelector('#laser');
-    this.camera = document.querySelector('#camera');
+    this.gameState = this.sceneEl.systems['game-state'];
     const data = this.data;
     this.material  = new THREE.ShaderMaterial({
       uniforms: {
         u_time: { value: 0.0 },
         color: { value: new THREE.Color(data.color) },
-        u_controllerPos: {value: this.camera.object3D.position/*this.magicLight.object3D.position*/},
+        u_controllerPos: {value: this.gameState.camera.object3D.position},
+        u_stonePos: {value: this.gameState.lostStone.object3D.position}
       },
       vertexShader,
       fragmentShader,
-      transparent: true
+      transparent: true,
     });
     this.applyToMesh();
     this.el.addEventListener('model-loaded', () => this.applyToMesh());
@@ -65,8 +69,8 @@ let labyrinthMaterial = AFRAME.registerComponent('labyrinth-material', {
     // console.log(JSON.stringify(this.camera.object3D.position));
 
     this.material.uniforms.u_time.value = t / 1000;
-    this.material.uniforms.u_controllerPos.value = this.camera.object3D.position;//this.magicLight.object3D.position;
-    // console.log(this.camera.object3D.position.z);
+    this.material.uniforms.u_stonePos.value = this.gameState.lostStone.object3D.position;
+    this.material.uniforms.u_controllerPos.value = this.gameState.camera.object3D.position;
 
     // add more uniforms like magic light dir and pos
   }
